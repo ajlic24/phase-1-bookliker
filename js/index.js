ae8fe6c9c5827018b736d2738d1fd31d398e6c47
ajlic24@gmail.com <ajlic24@gmail.com>
2021-12-16 13:31:35 -0500

    progress so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    let ul = document.getElementById(`list`)
+    let divPanel = document.getElementById(`show-panel`)
+
+    function removeChildNodes(parent) {
+        while(parent.firstChild){
+            parent.firstChild.remove()
+        }
+    }
+    
+    fetch(`http://localhost:3000/books`)
+    .then(resp => resp.json())
+    .then(data => {
+        data.forEach(({title, users, description, img_url}) => {
+            let li = document.createElement(`li`)
+            let btn = document.createElement(`button`)
+
+            li.textContent = title
+            btn.textContent = `LIKE`
+            li.addEventListener(`click`, () => {
+                removeChildNodes(divPanel)
+                let img = document.createElement(`img`)
+                let desc = document.createElement(`p`)
+                let userList = document.createElement(`ul`)
+                
+                img.src = img_url
+                desc.textContent = description
+                users.forEach((user) => {
+                    let userLi = document.createElement(`li`)
+                    userLi.textContent = user.username
+                    userList.appendChild(userLi)
+                })
+                btn.addEventListener(`click`, () => {
+
+                })
+                divPanel.append(img, desc, userList, btn)
+            })
+            ul.appendChild(li)
+        });
+        
+    })
+});
